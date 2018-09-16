3f5283d42f600d0efe9d2c84d45c044c2649da1b
Mihaylova Yuliya <yulu.2000@hotmail.com>
2018-09-17 00:50:43 +0300

    HW_03

--- HW_03/Task1.java
+++ HW_03/Task1.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
-
-public class Task1{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input a number: ");
         int n = sc.nextInt();
         System.out.println("Input array: ");
         int arr[] = new int[n];
-        int count =0;
-        for(int i = 0; i<n; i++){
+        int count = 0;
+        int result = 0;
+        int midHight = 0;
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
+            midHight = midHight + arr[i+1];
+            result = midHight/n;
+            if(arr[i] > result){
+                count = count + 1;
+            }
         }
-        if(arr[i]>arr[i+1] && arr[i]>arr[i-1]){
-            count = count + 1;
-        }
+        System.out.println(result);
         System.out.println(count);
     }
 }
