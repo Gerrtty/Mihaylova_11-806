20d01a95da47e01e326a935c168345ddf908576e
Mihaylova Yuliya <yulu.2000@hotmail.com>
2018-10-02 18:08:57 +0300

    FibonachiRecursion

---
+++ FibonachiRecursion.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int f1 = 0;
+        int f2 = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input n: ");
+        int n = sc.nextInt();
+        if(fibonachi(n, f1, f2) == 0){
+            System.out.println("This number not included in the fibonacci sequence");
+        }
+        else {
+            System.out.println("This number included in the fibonacci sequence");
+        }
+
+    }
+    public static int fibonachi(int n, int f1, int f2){
+        if(n > f2){
+            f1 +=f2;
+            f2 +=f1;
+            return fibonachi(n, f1, f2);
+        }
+        else if(n == f1 || n == f2){
+            return 1;
+        }
+        else return 0;
+    }
+
+}
