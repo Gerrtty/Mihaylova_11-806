d939fac1ac90c2deb2fc96973a0c04edb47a65b5
Mihaylova Yuliya <yulu.2000@hotmail.com>
2018-12-15 19:08:44 +0300

    Generic LinkedList

---
+++ HW_18/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> ml = new MyLinkedList<>();
+        ml.add(6);
+        ml.add(7);
+        MyLinkedList<Integer> m2 = new MyLinkedList<>();
+        m2.add(8);
+        m2.add(10);
+        ml.addAll(m2, 1);
+        for(int i = 0; i < ml.size(); i++){
+            System.out.println(ml.get(i));
+        }
+        //System.out.println(ml.contains(10));
+    }
+}
