4eb319b599c8d28778d907caf6b3796bb3c19c5d
Mihaylova <yulu.2000@hotmail.com>
2019-04-04 15:42:07 +0300

    Reflection

--- Semestr2/HW_34/src/Main.java
+++ Semestr2/HW_34/src/Main.java
@@ -2,23 +2,51 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
         getMany(Student.class, "John" , 18).stream().forEach(s -> System.out.println(s));
         System.out.println();
-        getMany(Student.class).stream().forEach(s -> System.out.println(s));
+        Integer age = 19;
+        getMany(Student.class, "Kris", age).stream().forEach(s -> System.out.println(s));
     }
 
-    public static <T> List <T> getMany(Class<T> c, Object... params) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+    public static <T> List <T> getMany(Class<T> c, Object... params) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         int count = 10, length = params.length;
         List<T> l = new ArrayList<>();
         Class[] arr = new Class[length];
         for (int i = 0; i < length; i++) {
             arr[i] = params[i].getClass();
         }
-        Constructor constructor = c.getConstructor(arr);
-        for (int i = 0; i < count; i++) {
-            T o = (T) constructor.newInstance(params);
-            l.add(o);
+        try {
+            Constructor constructor = c.getConstructor(arr);
+        }
+        catch (NoSuchMethodException e){
+            for (int i = 0; i < length; i++) {
+                if(params[i].getClass() == Integer.class){
+                    arr[i] = int.class;
+                }
+                else if(params[i].getClass() == Double.class){
+                    arr[i] = double.class;
+                }
+                else if(params[i].getClass() == Long.class){
+                    arr[i] = long.class;
+                }
+                else if(params[i].getClass() == Boolean.class){
+                    arr[i] = boolean.class;
+                }
+                else if(params[i].getClass() == Character.class){
+                    arr[i] = char.class;
+                }
+            }
+        }
+        try {
+            Constructor constructor = c.getConstructor(arr);
+            for (int i = 0; i < count; i++) {
+                T o = (T) constructor.newInstance(params);
+                l.add(o);
+            }
+        }
+        catch (NoSuchMethodException e){
+            throw new IllegalAccessException();
         }
         return l;
     }
--- Semestr2/HW_34/src/Student.java
+++ Semestr2/HW_34/src/Student.java
@@ -2,6 +2,11 @@ public class Student {
     private int age;
     private String name;
 
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
     public Student(String name, Integer age){
         this.name = name;
         this.age = age;
